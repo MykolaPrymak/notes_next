1d553c96989b0112a3928dffa76b611a9170f349
Mykola Prymak <mykola.prymak@gmail.com>
2023-05-15 00:21:35 +0200

    feat:  add mui blog template app

--- services/web/spa/src/containers/Application/Application.tsx
+++ services/web/spa/src/containers/Application/Application.tsx
@@ -1,11 +1,44 @@
 import React from "react";
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
 import "./application.css";
 
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+import Blog from "../Blog/Blog";
+
+const Root: React.FC<Record<string, never>> = () => <>
+  <Container maxWidth="sm">
+
+    <Box sx={{ my: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        <Link to="/">
+          React Application
+        </Link>
+        <Link to="/page_a"> | Page A</Link>
+        <Link to="/page_b"> | Page B</Link>
+        <Link to="/blog"> | Blog</Link>
+
+
+        <Outlet />
+      </Typography>
+    </Box>
+  </Container>
+
+</>;
 
 const Application: React.FC<Record<string, never>> = () => {
-
   return (
-    <h1>React Application</h1>
+    <BrowserRouter  >
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route path="page_a" element={<p>Page A Content</p>} />
+          <Route path="page_b" element={<p>Page B Content</p>} />
+        </Route>
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- services/web/spa/src/index.tsx
+++ services/web/spa/src/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import Application from "./containers/Application";
+import theme from './containers/Application/theme';
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
-root.render(<Application />);
+
+root.render(<ThemeProvider theme={theme}>
+    <CssBaseline />
+    <React.StrictMode>
+        <Application />
+    </React.StrictMode>
+</ThemeProvider>
+);
