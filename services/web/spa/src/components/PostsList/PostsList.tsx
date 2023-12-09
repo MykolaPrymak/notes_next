5c053e48b4c0f3f308e66fe882f4412a032a8751
Mykola Prymak <mykola.prymak@gmail.com>
2023-12-09 22:59:05 +0100

    feat: show post tags

--- services/web/spa/src/components/PostsList/PostsList.tsx
+++ services/web/spa/src/components/PostsList/PostsList.tsx
@@ -5,6 +5,8 @@ import Divider from '@mui/material/Divider';
 import Markdown from '../Markdown';
 import { Post } from '../../store/slices/posts'
 import { Link } from 'react-router-dom';
+import Chip from '@mui/material/Chip';
+import Pagination from '@mui/material/Pagination';
 
 export interface PostsListProps {
   posts: ReadonlyArray<Post>;
@@ -25,8 +27,29 @@ export default function PostsList(props: PostsListProps) {
         '& .post-item': {py: 2},
         '& .post-date-author': {mb: 2, fontStyle: 'italic'},
         '& .markdown': {
-          py: 0,
+          display: "block", // TODO: Use container to show markdown box shadow
+          pt: 0,
+          pb: 1,
+          mb: 2,
+          maxHeight: "5em",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          borderRadius: "0 0 15px 15px",
         },
+        '& .markdown.clipped': {
+          position: "relative",
+        },
+        '& .markdown.clipped::after': {
+          display: "block",
+          width: "100%",
+          height: 0,
+          position: "absolute",
+          background: "transparent",
+          bottom: "1em",
+          left: 0,
+          content: `" "`,
+          boxShadow: "0 3em 22px 37px #fff"
+        }
       }}
     >
       <Typography variant="h6" gutterBottom>
@@ -39,11 +62,13 @@ export default function PostsList(props: PostsListProps) {
 
           <Typography className="post-date-author"><em>{post.created_at} by <Link to={`/author/${post.author.id}`}>{post.author.username}</Link></em></Typography>
 
-          <Markdown className="markdown">
+          <Markdown className="markdown clipped">
             {post.body}
           </Markdown>
+          {post.tags.map(tag => <Chip key={`${post.id}_${encodeURIComponent(tag)}`} label={tag} component="a" href={`/?tag=${encodeURIComponent(tag)}`} clickable />)}          
         </div>
       ))}
+      <Pagination count={10} size="large" shape="rounded" />
     </Grid>
   );
 }
